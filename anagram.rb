3f69075c18be8850e0111cf51fad57005efc3428
Anjali Shenoy <anjshenoy@gmail.com>
2012-04-09 16:13:40 -0700

    different way of finding anagrams

--- anagram.rb
+++ anagram.rb
@@ -17,7 +17,30 @@ class Anagram
       puts value.join(", ")
     end
   end
+
+  def self.lookup(words=[])
+    anagrams = {}
+    words.each do |word|
+      hash = {}
+      word.chars.sort.each do |c|
+        if hash.has_key?(c)
+          hash[c] += 1
+        else
+          hash[c] = 0
+        end
+      end
+      if anagrams.has_key?(hash)
+        anagrams[hash] << word
+      else
+        anagrams[hash] = [word]
+      end
+    end
+    anagrams.each_pair do |hash, words|
+      puts words.inspect if words.size > 1
+    end
+  end
 end
 
 words = %w(key fek god dog goo doo asd das hello yello)
-Anagram.find_in_list(words)
+#Anagram.find_in_list(words)
+Anagram.lookup(words)
