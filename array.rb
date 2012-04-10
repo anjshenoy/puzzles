ec54a15ff49c19210cd38a3066b3c4409a6cf333
Anjali Shenoy <anjshenoy@gmail.com>
2012-04-09 20:32:08 -0700

    make the corresponding row and column of every zeroeth element zero

--- array.rb
+++ array.rb
@@ -29,7 +29,37 @@ class Array
   def rotate_clockwise
     self.transpose.swap_rows
   end
+
+  # for every element encoutered that is 0, make its row and column zero
+  def custom_zero_row_col
+    rows = self.size - 1
+    zero_positions = []
+    (0..rows).each do |r|
+      cols = self[r].size - 1
+      (0..cols).each do |c|
+        if self[r][c] == 0
+          zero_positions << [r,c]
+        end
+      end
+    end
+    zero_positions.each do |arr|
+      self.zeroify(arr)
+    end
+    self
+  end
+
+  def zeroify(array)
+    r,c = array
+    (0...self[r].size).each do |col|
+      self[r][col] = 0
+    end
+    (0...self.size).each do |row|
+      self[row][c] = 0
+    end
+  end
 end
 
-puts [[1,2],[3,4]].rotate_clockwise.inspect
-puts [[1,2,3], [4,5,6], [7,8,9]].rotate_clockwise.inspect
+#puts [[1,2],[3,4]].rotate_clockwise.inspect
+#puts [[1,2,3], [4,5,6], [7,8,9]].rotate_clockwise.inspect
+#
+puts [[1,0,2], [3,4,5], [6,7,9]].custom_zero_row_col.inspect
