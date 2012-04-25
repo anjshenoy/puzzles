bccf58a15229a7328d8379ed57e3c257a7ddd745
Anjali Shenoy <anjshenoy@gmail.com>
2012-04-24 20:22:55 -0700

    string permutations

--- sets.rb
+++ sets.rb
@@ -7,4 +7,17 @@ def subsets(set)
   return ss.sort.uniq
 end
 
-puts subsets([1,2,3]).inspect
+#puts subsets([1,2,3]).inspect
+
+def perm(str)
+  return [str] if str.size == 1 || str.empty?
+  result = []
+  (0...str.size).each do |i|
+    perm(str[0...i] + str[i+1...str.size]).each do |s|
+      result <<  s + str[i]
+    end
+  end
+  result
+end
+
+puts perm("abc").inspect
